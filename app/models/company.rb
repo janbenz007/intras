97721f5c874f760ebd40e9489cccae96b83cb8e6
Jan Benz <jan.benz@email.cz>
2021-07-10 19:24:21 +0000

    Added Companies migration and model

---
+++ app/models/company.rb
@@ -0,0 +1,5 @@
+class Company < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :origin, presence: true, length: { is: 3 }
+  validates :eori, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_190614) do
+ActiveRecord::Schema.define(version: 2021_07_10_191104) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_07_10_190614) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "eori"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "origin"
+    t.string "eori"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", force: :cascade do |t|
