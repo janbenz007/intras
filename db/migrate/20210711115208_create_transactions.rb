989117c08b73c9841e472f7ea69659f06de56e14
Jan Benz <jan.benz@email.cz>
2021-07-11 12:44:32 +0000

    reshaped transaction migration to add new columns

--- db/migrate/20210711115208_create_transactions.rb
+++ db/migrate/20210711115208_create_transactions.rb
@@ -1,7 +1,16 @@
 class CreateTransactions < ActiveRecord::Migration[6.1]
   def change
     create_table :transactions do |t|
-
+      t.integer :classification_id
+      t.integer :year
+      t.integer :month
+      t.integer :day
+      t.integer :quantity
+      t.integer :unitprice
+      t.string :quantityunit # states the measure of quanity such as litre or kilogram
+      t.string :unitpricefx # currency the price is in such as EUR, CZK or PLN
+      t.integer :exporter_id
+      t.integer :importer_id
       t.timestamps
     end
   end
